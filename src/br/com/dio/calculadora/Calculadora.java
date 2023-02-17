6d338b4967bf0a936cb2f11f37f7ce24c3b39401
Sukita <lucassilva1001@hotmail.com>
2023-02-16 22:26:45 -0300

    Criação de calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,54 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double a, b;
+
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double divisao = divisao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+
+		System.out.println("soma " + soma);
+		System.out.println("sub " + subtracao);
+		System.out.println("div " + divisao);
+		System.out.println("mult " + multiplicacao);
+
+	}
+
+	public static double soma(double a, double b) {
+		return a + b;
+
+	}
+
+	public static double subtracao(double a, double b) {
+		return a - b;
+
+	}
+
+	public static double divisao(int a, int b) {
+		return a / b;
+
+	}
+
+	public static double divisao(double a, double b) {
+		return a / b;
+
+	}
+
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+
+	}
+
+}
